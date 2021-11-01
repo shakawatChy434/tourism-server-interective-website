70e2187de2317e7754d6914aae367652f3546342
sakawatchy434 <hossainshakawat434@gmail.com>
2021-11-01 17:36:33 +0600

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
-const app = express()
+const app = express();
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 
@@ -21,7 +22,42 @@ async function run() {
         console.log('Databasse connected');
         const database = client.db("touristDB");
         const touristCollection = database.collection("tourist");
+        const orderCollection = database.collection("orders");
         // query for movies that have a runtime less than 15 minutes
+
+        app.get('/services', async (req, res) => {
+            const cursor = touristCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await touristCollection.insertOne(service);
+            res.json(result);
+        });
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await touristCollection.findOne(query);
+            res.json(service);
+        });
+
+        // GET Manage all orders API
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        // POST Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        });
 
 
     } finally {
